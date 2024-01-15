2910741592c0f23224b670a8fec244e4c8856cc7
l0st-idi0t <carlnod121@gmail.com>
2024-01-14 20:51:39 -0600

    epic fail

--- sketch.js
+++ sketch.js
@@ -1,3 +1,69 @@
+class Cloth {
+  constructor(width, height, spacing, particleMass) {
+    this.width = width;
+    this.height = height;
+    this.spacing = spacing;
+    this.particleMass = particleMass;
+    this.particles = [];
+    this.sticks = [];
+    this.numCols = Math.floor(width / spacing);
+    this.numRows = Math.floor(height / spacing);
+  }
+
+  createCloth() {
+    for (let i = 0; i < this.numCols; i++) {
+      for (let j = 0; j < this.numRows; j++) {
+        let particle = new Particle(
+          i * this.spacing,
+          j * this.spacing,
+          this.particleMass
+        );
+        this.particles.push(particle);
+      }
+    }
+
+    for (let i = 0; i < this.numCols; i++) {
+      for (let j = 0; j < this.numRows; j++) {
+        if (i < this.numCols - 1) {
+          let stick = new Stick(
+            this.particles[i * this.numCols + j],
+            this.particles[(i + 1) * this.numCols + j],
+            this.spacing
+          );
+          this.sticks.push(stick);
+        }
+
+        if (j < this.numRows - 1) {
+          let stick = new Stick(
+            this.particles[i * this.numCols + j],
+            this.particles[i * this.numCols + j + 1],
+            this.spacing
+          );
+          this.sticks.push(stick);
+        }
+
+        if (i < this.numCols - 1 && j < this.numRows - 1) {
+          let stick = new Stick(
+            this.particles[i * this.numCols + j],
+            this.particles[(i + 1) * this.numCols + j + 1],
+            this.spacing * Math.sqrt(2)
+          );
+          this.sticks.push(stick);
+        }
+
+        if (i < this.numCols - 1 && j < this.numRows - 1) {
+          let stick = new Stick(
+            this.particles[(i + 1) * this.numCols + j],
+            this.particles[i * this.numCols + j + 1],
+            this.spacing * Math.sqrt(2)
+          );
+          this.sticks.push(stick);
+        }
+      }
+    }
+  }
+}
+
 class Particle {
   constructor(x, y, mass) {
     this.x = x;
@@ -43,23 +109,29 @@ function getLength(v) {
   return Math.sqrt(v.x * v.x + v.y * v.y);
 }
 
+let cloth;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  let middleX = windowWidth / 2;
-  let middleY = windowHeight / 2;
-  let offset = 50;
+  // let middleX = windowWidth / 2;
+  // let middleY = windowHeight / 2;
+  // let offset = 50;
 
-  let pA = new Particle(middleX, middleY, 10000);
-  let pB = new Particle(middleX + offset, middleY, 10000);
-  let pC = new Particle(middleX, middleY + offset, 10000);
-  let pD = new Particle(middleX + offset, middleY + offset, 10000);
-  particles.push(pA, pB, pC, pD);
+  // let pA = new Particle(middleX, middleY, 10000);
+  // let pB = new Particle(middleX + offset, middleY, 10000);
+  // let pC = new Particle(middleX, middleY + offset, 10000);
+  // let pD = new Particle(middleX + offset, middleY + offset, 10000);
+  // particles.push(pA, pB, pC, pD);
 
-  let stickAB = new Stick(pA, pB, getDistance(pA, pB));
-  let stickDC = new Stick(pD, pC, getDistance(pB, pD));
-  let stickCA = new Stick(pC, pA, getDistance(pC, pA));
-  let stickDB = new Stick(pD, pB, getDistance(pD, pB));
-  sticks.push(stickAB, stickDC, stickCA, stickDB);
+  // let stickAB = new Stick(pA, pB, getDistance(pA, pB));
+  // let stickDC = new Stick(pD, pC, getDistance(pB, pD));
+  // let stickCA = new Stick(pC, pA, getDistance(pC, pA));
+  // let stickDB = new Stick(pD, pB, getDistance(pD, pB));
+  // sticks.push(stickAB, stickDC, stickCA, stickDB);
+  cloth = new Cloth(500, 500, 20, 10000);
+  cloth.createCloth();
+  particles = cloth.particles;
+  sticks = cloth.sticks;
 }
 
 function update() {
@@ -110,7 +182,7 @@ function draw() {
   update();
 
   for (let i = 0; i < particles.length; i++) {
-    circle(particles[i].x, particles[i].y, 10);
+    circle(particles[i].x, particles[i].y, 1);
   }
 
   for (let i = 0; i < sticks.length; i++) {
